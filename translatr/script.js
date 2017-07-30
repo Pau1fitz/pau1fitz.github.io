66cdd12c0d826cd3c8438797696b85ee67ff6c6c
Paul Fitzgerald <paulfitz99@gmail.com>
2017-07-30 13:48:15 +0100

    setup translatr

---
+++ translatr/script.js
@@ -0,0 +1,33 @@
+let voiceSpeak;
+let recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
+recognition.lang = 'en-US';
+recognition.interimResults = false;
+recognition.maxAlternatives = 5;
+window.speechSynthesis.onvoiceschanged = function() {
+    let voices = window.speechSynthesis.getVoices();
+    voices.forEach(voice => {
+      if(voice.name === 'Monica') {
+        voiceSpeak = voice
+      }
+    })
+};
+
+recognition.onresult = event => {
+    let voices = speechSynthesis.getVoices();
+    let translate = event.results[0][0].transcript;
+    fetch(`https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170729T221010Z.5c4f9485fbc7dffe.e8f1afc5701d0f8d4ecaaac9412f103d04d83b9c&text=${translate}&lang=en-es`).then(response =>{
+      return response.json();
+    }).then(response => {
+      let utterance  = new SpeechSynthesisUtterance();
+      utterance.voice = voiceSpeak;
+      utterance.text = response.text[0];
+      utterance.lang = 'es';
+      speechSynthesis.speak(utterance);
+      document.querySelector(".response").textContent = response.text[0];
+    });
+};
+
+let startRecording = () => {
+    recognition.start();
+}
+document.querySelector("button").addEventListener('click', startRecording);
