3323a5ed613b166f40ff95a89c9e86cf0b34923d
Paul Fitzgerald <paulfitz99@gmail.com>
2017-07-30 17:50:05 +0100

    animate the mouth

--- translatr/script.js
+++ translatr/script.js
@@ -1,5 +1,6 @@
 let voiceSpeak;
 let recognition;
+let utterance;
 
 if(window.SpeechRecognition == undefined && window.webkitSpeechRecognition == undefined && window.mozSpeechRecognition == undefined && window.msSpeechRecognition == undefined) {
   alert('Please try on Google Chrome or any other browser that supports Speech Recognition');
@@ -24,14 +25,24 @@ recognition.onresult = event => {
     fetch(`https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170729T221010Z.5c4f9485fbc7dffe.e8f1afc5701d0f8d4ecaaac9412f103d04d83b9c&text=${translate}&lang=en-es`).then(response =>{
       return response.json();
     }).then(response => {
-      let utterance  = new SpeechSynthesisUtterance();
+      utterance  = new SpeechSynthesisUtterance();
       utterance.voice = voiceSpeak;
       utterance.text = response.text[0];
       utterance.lang = 'es';
       speechSynthesis.speak(utterance);
       document.querySelector(".response").textContent = response.text[0];
+      document.querySelector(".mouth-top").className += " top-speak";
+      document.querySelector(".mouth-bottom").className += " bottom-speak";
+      utterance.onend = event => {
+        document.getElementById("top").className = document.getElementById("top").className.replace(/top-speak/,'');
+        document.getElementById("bottom").className =document.getElementById("bottom").className.replace(/bottom-speak/,'');
+      }
     });
+
+
 };
+
+
 
 let startRecording = () => {
     recognition.start();
